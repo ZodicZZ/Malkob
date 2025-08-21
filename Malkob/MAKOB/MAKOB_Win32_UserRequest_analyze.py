a14ae271e4a89682152ae81f6d0724568a23d7a7
Zodic <zodicffs@gmail.com>
2025-08-21 17:54:59 +0300

    Delete Malkob directory

--- Malkob/MAKOB/MAKOB_Win32_UserRequest_analyze.py
+++
@@ -1,8 +0,0 @@
-import simple_colors
-import MAKOB.MAKOB_Win32_Requests_engine as MALKOB_Win32_Requests_engine
-def MALKOB_user_Request(Request):
-    MALKOB_valid_Requests = ["ehelp", "ecreate", "elisten", "edump", "esettings", "cls", "eclose"]
-    if(Request not in MALKOB_valid_Requests):
-        print(simple_colors.red("[Error] Request not found."))
-    elif(Request in MALKOB_valid_Requests):
-        MALKOB_Win32_Requests_engine.MALKOB_requests_engine(Request)
